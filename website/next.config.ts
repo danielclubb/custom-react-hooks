3ea005c269cab6fca1bc5fce0229a771b954c44e
danielclubb <danieljohnclubb@gmail.com>
2026-06-08 15:31:33 +0400

    fix: vercel deploy

--- website/next.config.ts
+++ website/next.config.ts
@@ -1,11 +1,17 @@
 import type { NextConfig } from "next";
 import path from "path";
 
+const isDev = process.env.NODE_ENV === "development";
+
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: path.join(process.cwd(), "../"),
-  outputFileTracingIncludes: {
-    "/docs/[hookName]": ["../hooks/**/*"],
-  },
+  ...(isDev
+    ? {}
+    : {
+        outputFileTracingRoot: path.join(process.cwd(), "../"),
+        outputFileTracingIncludes: {
+          "/docs/[hookName]": ["../hooks/**/*"],
+        },
+      }),
 };
 
 export default nextConfig;
