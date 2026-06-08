d69ae81e43dfcaa85bde84df1e0843c78febe1a0
danielclubb <danieljohnclubb@gmail.com>
2026-06-08 13:10:21 +0400

    feat: add useCounter hook

---
+++ hooks/__tests__/useCounter.test.tsx
@@ -0,0 +1,40 @@
+import {act, renderHook} from '@testing-library/react';
+
+import useCounter from '../useCounter';
+
+describe('useCounter', () => {
+    test('increment method', () => {
+        const {result} = renderHook(() => useCounter());
+
+        expect(result.current.count).toBe(0);
+
+        act(() => {
+            result.current.increment();
+        });
+        
+        expect(result.current.count).toBe(1);
+    });
+
+    test('decrement method', () => {
+        const {result} = renderHook(() => useCounter(2));
+
+        act(() => {
+            result.current.decrement();
+        });
+        
+        expect(result.current.count).toBe(1);
+    });
+
+    test('reset method', () => {
+        const {result} = renderHook(() => useCounter(10));
+
+        act(() => {
+            result.current.decrement();
+            result.current.decrement();
+            result.current.decrement();
+            result.current.reset();
+        });
+        
+        expect(result.current.count).toBe(10);
+    });
+});
---
+++ hooks/useCounter.tsx
@@ -0,0 +1,25 @@
+import {useState, useCallback} from 'react';
+
+interface Output {
+    count: number;
+    increment: () => void;
+    decrement: () => void;
+    reset: () => void;
+    setCount: (value: number) => void;
+}
+
+export default function useCounter(initialValue = 0) : Output {
+    const [count, setCount] = useState(initialValue);
+
+    const increment = useCallback(() => setCount(previous => previous + 1), []);
+    const decrement = useCallback(() => setCount(previous => previous - 1), []);
+    const reset = useCallback(() => setCount(initialValue), []);
+
+    return {
+        count,
+        increment,
+        decrement,
+        reset,
+        setCount,
+    }
+}
