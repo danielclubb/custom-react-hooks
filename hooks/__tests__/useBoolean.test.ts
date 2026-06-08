61f23898bf9c4b6edd9da668d14e2a78236d6699
danielclubb <danieljohnclubb@gmail.com>
2026-06-08 12:35:08 +0400

    feat: finish useBoolean test cases

--- hooks/__tests__/useBoolean.test.ts
+++ hooks/__tests__/useBoolean.test.ts
@@ -2,9 +2,37 @@ import { act, renderHook } from '@testing-library/react';
 
 import useBoolean from '../useBoolean';
 
-describe('useDefault', () => {
+describe('useBoolean', () => {
     test('no default value passed', () => {
         const {result} = renderHook(() => useBoolean());
+
+        expect(result.current.value).toBe(false);
+    });
+
+    test('default value true', () => {
+        const {result} = renderHook(() => useBoolean(true));
+
+        expect(result.current.value).toBe(true);
+    });
+
+    test('default value false', () => {
+        const {result} = renderHook(() => useBoolean(false));
+
+        expect(result.current.value).toBe(false);
+    });
+
+    test('setTrue method', () => {
+        const {result} = renderHook(() => useBoolean(false));
+
+        act(() => result.current.setTrue());
+
+        expect(result.current.value).toBe(true);
+    });
+
+    test('setFalse method', () => {
+        const {result} = renderHook(() => useBoolean(true));
+
+        act(() => result.current.setFalse());
 
         expect(result.current.value).toBe(false);
     });
